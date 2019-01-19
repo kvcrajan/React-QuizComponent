9fbb47a9afa52d614d55352700e8a9aefc0001f8
Rajarajan Veerichetty <rajarajan.v@gmail.com>
2019-01-19 18:11:06 -0500

    Completed new QuizEnd component module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import QuizQuestion from "./QuizQuestion.js";
+import QuizEnd from "./QuizEnd.js";
 
 let quizData = require("./quiz_data.json");
 
@@ -11,12 +12,17 @@ class Quiz extends Component {
 
   render() {
     const { quiz_position } = this.state;
+    const isQuizEnd = quiz_position - 1 === quizData.quiz_questions.length;
 
     return (
       <div>
-        <QuizQuestion
-          quiz_question={quizData.quiz_questions[quiz_position - 1]}
-        />
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={quizData.quiz_questions[quiz_position - 1]}
+          />
+        )}
       </div>
     );
   }

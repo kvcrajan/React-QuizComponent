207b0ec65942cf37700d27f1e012fcf6ac2c41cf
Rajarajan Veerichetty <rajarajan.v@gmail.com>
2019-01-19 18:42:01 -0500

    Completed Quiz Questions and Handling Events module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
-import QuizQuestionButton from './QuizQuestionButton.js';
+import QuizQuestionButton from "./QuizQuestionButton.js";
 
 class QuizQuestion extends Component {
+    handleClick(buttonText) {
+        if (buttonText === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler();
+        }
+    }
   render() {
     const { quiz_question } = this.props;
 
@@ -12,7 +17,11 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={quiz_question.answer_options[0]} />
+            {
+                quiz_question.answer_options.map((answer_option, index) => {
+                    return (<QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)} />);
+                })
+            }
           </ul>
         </section>
       </main>
